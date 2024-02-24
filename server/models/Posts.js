8021fdfc5bccd5fd8b46b7c9d4414e2b8dfd7e95
ArtisAdemi <artis.ademi@gmail.com>
2024-02-24 23:09:13 +0100

    Comments

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -16,5 +16,10 @@ module.exports = (sequelize, DataTypes) => {
 
     })
 
-    return Posts
-}
+    Posts.associate = (models) => {
+        Posts.hasMany(models.Comments, {
+            onDelete: "cascade",
+        });
+    };
+    return Posts;
+};
