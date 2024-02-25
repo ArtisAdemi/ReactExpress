7d94873c03187b77a5475f3df2effbdc6ca84375
ArtisAdemi <artis.ademi@gmail.com>
2024-02-25 21:34:07 +0100

    Login function in Ui

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const login = () => {
+        const data = { username: username, password: password };
+        axios.post("http://localhost:3001/auth/login", data).then((res) => {
+            console.log(res.data);
+        });
+    };
+
+    return (
+        <div className="loginContainer">
+        <label>Username:</label>
+        <input
+          type="text"
+          onChange={handleUsernameChange}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={handlePasswordChange}
+        />
+  
+        <button onClick={login}> Login </button>
+      </div>
+    );
 }
 
-export default Login
+export default Login;
