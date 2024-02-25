e76a463aa7bc861cf9ae85212ee11aa6cbd11921
ArtisAdemi <artis.ademi@gmail.com>
2024-02-25 18:22:19 +0100

    Registration form with axios implemented in UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
 
 
 function App() {
@@ -13,11 +15,15 @@ function App() {
         <div className='navbar'>
         <Link to="createpost">Create a Post</Link>
         <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Signup</Link>
         </div>
         <Routes>
           <Route path='/' exact Component={Home} />
           <Route path='/createpost' exact Component={CreatePost} />
           <Route path='/post/:id' exact Component={Post} />
+          <Route path='/register' exact Component={Registration} />
+          <Route path='/login' exact Component={Login} />
         </Routes>
       </Router>
     </div>
